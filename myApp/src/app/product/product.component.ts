a9c7f04b53c530c5db736b9fd2de877d040a4f64
andreayang <yangyy@shinetechchina.com>
2018-04-03 17:49:32 +0800

    auction project

---
+++ myApp/src/app/product/product.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { Product } from './Product';
+
+@Component({
+  selector: 'app-product',
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.css']
+})
+export class ProductComponent implements OnInit {
+  public products: Array<Product>;
+  constructor() {
+  }
+
+  ngOnInit() {
+    this.products = [
+      new Product(1, '第一个商品', 899, 3.5, '这是一个电脑', ['电子产品', '家电']),
+      new Product(2, '第二个商品', 799, 2.5, '这是一个衣服', ['电子产品', '家电']),
+      new Product(3, '第三个商品', 859, 4.5, '这是一个垃圾电脑', ['电子产品', '家电']),
+      new Product(4, '第四个商品', 699, 3.5, '这是一个垃圾电脑', ['电子产品', '家电']),
+      new Product(5, '第五个商品', 899, 3.5, '这是一个垃圾电脑', ['电子产品', '家电']),
+      new Product(6, '第六个商品', 899, 3.5, '这是一个垃圾电脑', ['电子产品', '家电']),
+      new Product(7, '第七个商品', 699, 3.5, '这是一个垃圾电脑', ['电子产品', '家电']),
+      new Product(8, '第八个商品', 599, 3.5, '这是一个垃圾电脑', ['电子产品', '家电']),
+      new Product(9, '第九个商品', 199, 3.5, '这是一个垃圾电脑', ['电子产品', '家电']),
+    ];
+  }
+}
+
+
